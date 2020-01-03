4d0ac8d938e76901b78192d087c26a4c12dfb167
Josh Yopp <davidjyopp@gmail.com>
2020-01-03 15:04:25 -0500

    refactor of FT to use helper method

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User hears about todo list app and gose to check out its homepage at localhost
         self.browser.get('http://localhost:8000')
@@ -35,6 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         # '1: buy peacock feathers' In the todo list items
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -48,13 +54,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # page updates and the todo item list now shows both items
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # user wonders if the site will remember her list and sees that the site generated
         # a unique url for them --there is explanatory text that reflects this
