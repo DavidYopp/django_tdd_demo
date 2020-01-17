7eb217a97471f68f0bfc965ba3dad3481e1f944a
Josh Yopp <davidjyopp@gmail.com>
2020-01-17 11:13:28 -0500

    Testlist item validation tests written out implemented a new helper function that will help any function that we may need to wait for a response Test is in Red Phase

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,16 +6,33 @@ class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
         # User Goes to homepage and accidentally tries to submit empty list item
-        # They press the enter key while on teh empty input textbox
+        # They press the enter key while on the empty input textbox
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # The home page refreshes and there is an error message saying that list
         # list items cannot be blank
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
-        # User tries again with text for the tiem and it now works
+        # User tries again with text for the time and it now works
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
-        # user enters a blank item on'accident' again
+        # user enters a blank item on 'accident' again
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # user gets a warning again for blank items not allowed
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has_error').text,
+            "You can't have an empty list item"
+        ))
 
         # user can correct this by filling in some textbox
-        self.fail('!!Test is failing and you need to continue to write it!!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
