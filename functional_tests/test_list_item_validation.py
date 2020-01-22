a32fe8519e77c9ee12c9264965423c3bb3918c4f
Josh Yopp <davidjyopp@gmail.com>
2020-01-22 12:10:55 -0500

    implemented duplicate item validation at the model layer using the meta unique_together attribute unit tests are green

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -42,3 +42,21 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
+
+
+    def test_cannot_add_duplicate_items(self):
+        #user goes to home page and starts a new list
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy graphics card')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy graphics card')
+
+        #user accidentally enter a duplicate item
+        self.get_item_input_box().send_keys('Buy graphics card')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        #user gets an duplicate item error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has_error').text,
+            "You already have this item in your list"
+        ))
