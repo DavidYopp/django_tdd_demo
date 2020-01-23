d445d740c5e8ec75a72bb761a7bab7af0090402d
Josh Yopp <davidjyopp@gmail.com>
2020-01-23 10:07:02 -0500

    functional tests added for checking if error message disappears upon the user typing somethign in the in the textbox that doesnt match a list item

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,6 +4,10 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
+
     def test_cannot_add_empty_list_items(self):
         # User Goes to homepage and accidentally tries to submit empty list item
         # They press the enter key while on the empty input textbox
@@ -43,6 +47,7 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
 
+
     def test_cannot_add_duplicate_items(self):
         #user goes to home page and starts a new list
         self.browser.get(self.live_server_url)
@@ -56,6 +61,28 @@ class ItemValidationTest(FunctionalTest):
 
         #user gets an duplicate item error message
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You've already got this item in your list"
+        ))
+
+
+    def test_error_messages_are_cleared_on_input(self):
+        # user starts a list that causes a validation errors
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Seagulls Stop it now')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Seagulls Stop it now')
+        self.get_item_input_box().send_keys('Seagulls Stop it now')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # user starts typing in the input box again to clear the error error_messages
+        self.get_item_input_box().send_keys('a')
+
+        #user sees that the error message has gone away
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
